a465465b71368256726ea6f04be7001b48835741
pauleena phan <pauleena2002@gmail.com>
2023-06-06 16:52:02 -0700

    added popup form and adding to the library

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 let library = [{'title':"Harry Potter", 'author':"J.K. Rowling", 'date': "1990"}];
+let addbtn = document.getElementById("add");
+let form = document.getElementById("form");
+let form2 = document.getElementById("form2");
+let sub_btn = document.getElementById("submit");
+
+let title = document.getElementById("title").value;
+let author = document.getElementById("author").value;
+let date = document.getElementById("date").value;
 
 //creates the book
-function book(_title, _author, _date){
+function addbook(_title, _author, _date){
+    console.log("book function");
     library.push({title: _title, author: _author, date: _date});
 }
 
 //displays the book to the screen
 function addToLibrary(){
-    for(var i in library){
-        console.log(i.title, i.author, i.date);
+    for(var i = 0; i < library.length; i++){
+        //print out the values in the array || use dot notation to get specific attribute of arr
+        console.log(i);
+        console.log(library[i]);
     }
 }
+
+// when nthe user clicks on add book a popup form will show
+addbtn.addEventListener("click", ()=>{
+    form.style.visibility = "visible";
+});
+
+//after pressing the submit button users can put in their book info and add it to their librar
+sub_btn.addEventListener("click", (e)=>{
+    //prevents the form from sending stuff to the server
+    e.preventDefault(); 
+    addbook(this.title.value, this.author.value, this.date.value);
+    form.style.visibility = "hidden";
+    addToLibrary();
+
+    form2.reset();
+});
