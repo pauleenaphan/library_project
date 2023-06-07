3d5fe5ab6101f61b85392ae77f74946b1b4cf2ab
pauleena phan <pauleena2002@gmail.com>
2023-06-06 21:21:43 -0700

    added remove button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let library = [{'title':"Harry Potter", 'author':"J.K. Rowling", 'date': "1990"}];
+let library = [{'title':"Harry Potter", 'author':"J.K. Rowling", 'date': "1990", 'inlibrary': false}];
 let addbtn = document.getElementById("add");
 let form = document.getElementById("form");
 let form2 = document.getElementById("form2");
@@ -13,35 +13,42 @@ let body = document.getElementsByClassName("body_two");
 //creates the book
 function addbook(_title, _author, _date){
     console.log("book function");
-    library.push({title: _title, author: _author, date: _date});
+    library.push({title: _title, author: _author, date: _date, inlibrary: false});
 }
 
 //displays the book to the screen
 function addToLibrary(){
     for(var i = 0; i < library.length; i++){
-        //print out the values in the array || use dot notation to get specific attribute of arr
-        console.log(i);
-        console.log(library[i]);
+        //first checks whether or not the book is in the library
+        if(library[i].inlibrary == false){
+            //print out the values in the array || use dot notation to get specific attribute of arr
+            library[i].inlibrary = true;
 
-        let book = document.createElement('div');
-        book.className = "book";
-        //this property is 0 because we are choosing body_two which is the first element in that class
-        document.getElementsByClassName("body_two")[0].appendChild(book);
+            let book = document.createElement('div');
+            book.className = "book";
+            //this property is 0 because we are choosing body_two which is the first element in that class
+            document.getElementsByClassName("body_two")[0].appendChild(book);
 
-        let b_title = document.createElement('div');
-        b_title.className = "b_title";
-        b_title.textContent = (library[i].title);
-        book.appendChild(b_title);
+            let b_title = document.createElement('div');
+            b_title.className = "b_title";
+            b_title.textContent = (library[i].title);
+            book.appendChild(b_title);
 
-        let b_author = document.createElement('div');
-        b_author.className = "b_author";
-        b_author.textContent = (library[i].author);
-        book.appendChild(b_author);
+            let b_author = document.createElement('div');
+            b_author.className = "b_author";
+            b_author.textContent = (library[i].author);
+            book.appendChild(b_author);
 
-        let b_date = document.createElement('div');
-        b_date.className = "b_date";
-        b_date.textContent = (library[i].date);
-        book.appendChild(b_date);
+            let b_date = document.createElement('div');
+            b_date.className = "b_date";
+            b_date.textContent = (library[i].date);
+            book.appendChild(b_date);
+
+            let b_remove = document.createElement("button");
+            b_remove.className = "b_remove";
+            b_remove.textContent = "X";
+            book.appendChild(b_remove);
+        }
     }
 }
 
